87c90ad8fc8e333d88b7dc43ae7f41f4daf05a36
jimmy <JimmyMall@live.com>
2020-07-22 13:00:32 +0800

    main0.2.6-7 添加百度统计

--- static/js/tongji.js
+++ static/js/tongji.js
@@ -1,8 +1,9 @@
-//百度统计
+//百度统计 亲自走路
 var _hmt = _hmt || [];
 (function() {
-	var hm = document.createElement("script");
-	hm.src = "https://hm.baidu.com/hm.js?e8bfc4624d292f16f30358abd921d748";
-	var s = document.getElementsByTagName("script")[0]; 
-	s.parentNode.insertBefore(hm, s);
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?67fbab3917f79630bbdd3063162c78a2";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
 })();
+
